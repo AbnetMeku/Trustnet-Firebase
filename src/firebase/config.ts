ad001398558fdf949f49b85f8034e99a47775406
Abnet Mekuria <abnetmekuria07@gmail.com>
2026-01-01 15:57:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3741240562-49d32",
+  "appId": "1:375676965428:web:5d3c36310b600f272fb061",
+  "apiKey": "AIzaSyBEjU1Hdjs_iR1rFetYXeQMrlE2-hUgt-c",
+  "authDomain": "studio-3741240562-49d32.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "375676965428"
+};
